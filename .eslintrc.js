c32e7fa3d45e1f5f205ae7263dbc14c4c8ceecc9
ianlikono <ianlikono@gmail.com>
2017-12-14 10:03:16 +0300

    aggg

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,8 @@ module.exports = {
   plugins: ['react', 'jsx-a11y', 'import'],
   rules: {
     'react/jsx-filename-extension': 0,
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
   },
   globals: {
     document: 1,
